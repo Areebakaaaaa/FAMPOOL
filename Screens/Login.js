0018a9516ff9dacabfeb6d1c5cf0e8a6e17b4108
Fareeha Naz <fareehanaz31@gmail.com>
2024-01-08 23:59:10 +0500

    Project Base

---
+++ Screens/Login.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {View, Text, Touchable, TouchableOpacity} from 'react-native';
+
+const Login = (props) => {
+  return (
+      <View style={{alignItems: 'center', width: 460}}>
+        <Text
+          style={{
+            color: 'white',
+            fontSize: 64,
+            fontWeight: 'bold',
+            marginVertical: 20,
+          }}>
+          Login
+        </Text>
+        <View
+          style={{
+            backgroundColor: 'white',
+            height: 700,
+            width: 460,
+            borderTopLeftRadius: 130,
+            paddingTop: 100,
+            alignItems: 'center',
+          }}>
+          <Text style={{fontSize: 40, fontWeight: 'bold'}}>
+            Welcome Back
+          </Text>
+          <Text
+            style={{
+              color: 'grey',
+              fontSize: 19,
+              fontWeight: 'bold',
+              marginBottom: 20,
+            }}>
+            Login to your account
+          </Text>
+          <Text
+            placeholder="Email / Username"
+            keyboardType={'email-address'}
+          />
+          <Text placeholder="Password" secureTextEntry={true} />
+          <View
+            style={{alignItems: 'flex-end', width: '78%', paddingRight: 16, marginBottom: 200}}>
+            <Text style={{fontWeight: 'bold', fontSize: 16}}>
+              Forgot Password ?
+            </Text>
+          </View>
+          <Text textColor='white' btnLabel="Login" Press={() => alert("Logged In")} />
+          <View style={{ display: 'flex', flexDirection :'row', justifyContent: "center" }}>
+            <Text style={{ fontSize: 16, fontWeight:"bold" }}>Don't have an account ? </Text>
+            <TouchableOpacity onPress={() => props.navigation.navigate("Signup")}>
+            <Text style={{fontWeight: 'bold', fontSize: 16 }}>Signup</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+  );
+};
+
+export default Login;
