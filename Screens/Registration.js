b3d6bb389660f7a1f76b586187470c4930963407
Fareeha Naz <fareehanaz31@gmail.com>
2024-01-10 09:33:18 +0500

    registration screen created

---
+++ Screens/Registration.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import {View, Text, Touchable, TouchableOpacity} from 'react-native';
+
+export const Registration = () => {
+
+    return (
+        <View>
+
+
+        </View>
+    )
+}
