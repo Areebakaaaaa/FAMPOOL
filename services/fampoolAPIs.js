635bb3b8eb010437e64ee09872e970e4d2e6e1d6
Fareeha Naz <fareehanaz31@gmail.com>
2024-01-25 11:53:08 +0500

    network error solution?

--- services/fampoolAPIs.js
+++ services/fampoolAPIs.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 export const addUser = async (name) => {
     try{
+        console.log("Name passed: ",name);
         const res = await axios.post('http://localhost:5000/users/add-user', {
             name: name
         })
