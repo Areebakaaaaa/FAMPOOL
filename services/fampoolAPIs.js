7c5af4a290f4b887fab9c499200c5a1289dff016
Areeba Asad <k201609@nu.edu.pk>
2024-02-09 23:43:10 +0500

    kuch tou hua hai

--- services/fampoolAPIs.js
+++ services/fampoolAPIs.js
@@ -2,13 +2,30 @@ import axios from 'axios'
 
 export const addUser = async (name) => {
     try{
-        const res = await axios.post('http://localhost:5000/users/add-user', {
-            name: name
-        })
-        console.log(res)
+        console.log("Before API");
+        //console.log("Name: ", userDetails.name, "Rollno: ", userDetails.nuId);
+        const res = await axios.post('http://192.168.100.14:5000/users/add-user', userDetails)
+        console.log("After API");
 
     }catch(err){
         console.log(err)
     }
 }
+
+export const getUser = async (loginDetails) => {
+    try{
+        console.log("Login process API entered.");
+        console.log("NuID: ", loginDetails.nuEmail);
+        console.log("Password: ", loginDetails.password);
+        const res = await axios.post('http://192.168.100.14:5000/users/get-user', loginDetails);
+        console.log("Login API exit.");
+        return res.data;
+
+    }catch(err)
+    {
+        console.log(err);
+    }
+}
+
+
 
