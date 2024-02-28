ffff7484551efcd244ae2607404302936892c3d1
Fareeha Naz <fareehanaz31@gmail.com>
2024-02-29 01:20:59 +0500

    SAR PHAR DO MERA

--- Screens/AvailableRide.js
+++ Screens/AvailableRide.js
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, TextInput, ScrollView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const AvailableRidesScreen = () => {
+  const navigation = useNavigation();
   const [rides, setRides] = useState([]);
   const ipv4='http://192.168.100.14';
 
@@ -22,6 +24,11 @@ const AvailableRidesScreen = () => {
     fetchRides();
   }, []);
 
+  const hojaPlease = (item) => {
+    console.log("Hoja bhaee ma roojaongaaa ab!.");
+    navigation.navigate("Abc", {ride: item});
+  }
+
   return (
     <View style={styles.container}>
         <FlatList
@@ -29,9 +36,17 @@ const AvailableRidesScreen = () => {
         keyExtractor={item=>item.id}
         renderItem={({ item }) => (
           <View style={styles.rideCard}>
-            <Text style={styles.rideDetails}>
-              {item.customerType} - {item.date} - {item.time} 
-            </Text>
+            <TouchableOpacity style={styles.rideCard} onPress={()=> hojaPlease(item)}>
+              <Text style={styles.rideDetails}>
+                {item.customerType} - {item.toFromFast} - {item.toFromLocation}
+              </Text>
+              <Text style={styles.rideDetails}>
+                {item.seats}/4 seats 
+              </Text>
+              <Text style={styles.rideDetails}>
+                Departure Time - {item.departureTime} 
+              </Text>
+            </TouchableOpacity>
           </View>
         )} 
         />
@@ -115,7 +130,6 @@ const styles = StyleSheet.create({
   bookNowText: {
     color: 'white',
   },
-  // Additional styles can be added here
 });
 
 export default AvailableRidesScreen;
