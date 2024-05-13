7187f2aab9fdb0b7f5a1f0d858f5d8492d39bf59
Fareeha Naz <fareehanaz31@gmail.com>
2024-05-13 13:41:52 +0500

    pushhhhh

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -67,62 +67,6 @@ const { AdminFirestore } = require('../utils/db');
   }
 }
 
-  async function addRide(postRideDetails){
-    const usersCollection = AdminFirestore.collection('availableRides');
-
-    try{
-      const userDoc = await usersCollection.add({
-        driverId: postRideDetails.driverId,
-        customerType: postRideDetails.customerType,
-        toFromFast: postRideDetails.toFromFast, 
-        departureTime: postRideDetails.time, 
-        date: postRideDetails.date, 
-        seats: postRideDetails.seats,
-        toFromLocation: postRideDetails.toFromLocation,
-      })
-
-      console.log("Ride posted successfully with ID: ", userDoc.id);
-    }catch(err)
-    {
-      console.error("Error posting ride: ", err);
-    }
-
-  }
-  
-  const getAvailableRide = async (req, res) =>{
-    try{
-      const availableRideCollection= AdminFirestore.collection('availableRides');
-      const snapshot = await availableRideCollection.get();
-      const rides=[];
-      snapshot.forEach(doc => {
-        rides.push({id: doc.id, ... doc.data()});
-      });
-
-      console.log("Available Rides: ", rides);
-      res.status(200).send(rides);
-    } catch(err){
-      console.error('Error fetching available rides: ', err);
-      res.status(500).send('Error fetching available rides');
-    };
-  }
-
-  const getDemoLocation = async (req, res) =>{
-    try{
-      const demoLocationCollection= AdminFirestore.collection('demoLocation');
-      const snapshot = await demoLocationCollection.get();
-      const loc=[];
-      snapshot.forEach(doc => {
-        loc.push({id: doc.id, ... doc.data()});
-      });
-
-      console.log("Available Rides: ", loc);
-      res.status(200).send(loc);
-    } catch(err){
-      console.error('Error fetching available rides: ', err);
-      res.status(500).send('Error fetching available rides');
-    };
-  }
-  
-  module.exports = ({addUser, verifyUser, addRide, getAvailableRide, getDemoLocation});
+  module.exports = ({addUser, verifyUser});
 
   
